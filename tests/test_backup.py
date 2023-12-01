9cde8380961ea1e00dac9f45f07b0005e2dc8d20
Wakil <youssoufwakil@gmail.com>
2023-12-01 18:10:24 +0100

    Ajout Workflow Test

--- tests/test_backup.py
+++ tests/test_backup.py
@@ -1,31 +1,18 @@
+
 import unittest
+import subprocess
+import sys
 import os
-from backup import sources, destinations  # Import des chemins
 
-class TestPaths(unittest.TestCase):
+class TestBackupExecution(unittest.TestCase):
 
-    def test_paths_match_main_branch(self):
-        expected_sources = [
-            'C:\\Users\\username\\Documents\\',
-            'C:\\Users\\username\\Pictures\\',
-            'C:\\Users\\username\\Videos\\',
-            'C:\\Users\\username\\Music\\',
-            'C:\\Users\\username\\Favorites\\',
-            'C:\\Users\\username\\Downloads\\',
-        ]
+    def test_script_execution(self):
+        script_path = "C:\\Python\\Python311\\Projet Backup\\backup.py"
 
-        expected_destinations = [
-            'C:\\username_BackUp\\Documents_username_Baaackup',
-            'C:\\username_BackUp\\Images_username_Backup',
-            'C:\\username_BackUp\\Vidéos_username_Backup',
-            'C:\\username_BackUp\\Musique_username_Backup',
-            'C:\\username_BackUp\\Favoris_username_Backup',
-            'C:\\username_BackUp\\Téléchargements_username_Backup',
-        ]
-
-        # Vérifier que les chemins correspondent à ceux de la branche main
-        self.assertEqual(sources, expected_sources)
-        self.assertEqual(destinations, expected_destinations)
+        try:
+            subprocess.run([sys.executable, script_path], check=True)
+        except subprocess.CalledProcessError as e:
+            self.fail(f"Script execution failed with return code {e.returncode}")
 
 if __name__ == '__main__':
     unittest.main()
